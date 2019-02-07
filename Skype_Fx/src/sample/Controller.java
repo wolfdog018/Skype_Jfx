150722c5640f07b39f86b1f3c589e1417390a3f8
wolfdog018 <wolf018dog@gmail.com>
2019-02-07 13:31:50 +0530

    Skype Jfx

--- Skype_Fx/src/sample/Controller.java
+++ Skype_Fx/src/sample/Controller.java
@@ -1,4 +1,46 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    double x,y;
+    @FXML
+    void close(MouseEvent event) {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+
+    }
+
+    @FXML
+    void dragged(MouseEvent event) {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+
+    }
+
+    @FXML
+    void max(MouseEvent event) {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint(" ");
+
+    }
+
+    @FXML
+    void min(MouseEvent event) {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+
+    }
+
+    @FXML
+    void press(MouseEvent event) {
+        x = event.getSceneX();
+        y = event.getSceneY();
+    }
 }
